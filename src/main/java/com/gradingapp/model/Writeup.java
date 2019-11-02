5fe259cabd9c43a46eee1b7f09e42e4391d8f35d
PriyankaDwivedi23 <you@example.com>
2019-11-01 20:18:25 -0400

    nothing to commit

---
+++ src/main/java/com/gradingapp/model/Writeup.java
@@ -0,0 +1,14 @@
+package com.gradingapp.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Writeup {
+	
+	String _id;
+	String homeworkName;
+	String writeUp;
+	
+	
+
+}
